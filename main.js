0ff79f2ab1bab1744d9afd11e38a73fff71e64e8
ClarmontKa <153301634+ClarmontKa@users.noreply.github.com>
2024-04-19 10:37:50 -0400

    A calculator

---
+++ main.js
@@ -0,0 +1,18 @@
+console.log ('Kaylee Clarmont');
+console.log ('19 APR 2024');
+console.log ('JS Simple Calculator');
+
+console.log ('OUTPUT:');
+
+const numb1 = Number (prompt('Please Enter a Number'));
+const numb2 = Number (prompt('Please Enter another Number'));
+
+const Total1 = numb1 + numb2;
+const Total2 = numb1 - numb2;
+const Total3 = numb1 * numb2;
+const Total4 = numb1 / numb2;
+
+console.log(`${numb1} + ${numb2} = ${Total1}`);
+console.log(`${numb1} - ${numb2} = ${Total2}`);
+console.log(`${numb1} * ${numb2} = ${Total3}`);
+console.log(`${numb1} / ${numb2} = ${Total4}`);
